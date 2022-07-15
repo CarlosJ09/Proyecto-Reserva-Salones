6e4dd0d93746b27224bad1e184215a5b947ae4c5
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2022-07-15 17:33:26 -0400

    Arreglando Bugs

---
+++ Reserva-Salones/Reserva-Salones/Controllers/FormulariosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Reserva_Salones;
+
+namespace Reserva_Salones.Controllers
+{
+    public class FormulariosController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Formularios
+        public ActionResult Index()
+        {
+            var formularios = db.Formularios.Include(f => f.TipoSalon);
+            return View(formularios.ToList());
+        }
+
+        // GET: Formularios/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Formulario formulario = db.Formularios.Find(id);
+            if (formulario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(formulario);
+        }
+
+        // GET: Formularios/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo");
+            return View();
+        }
+
+        // POST: Formularios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Cedula,Nombre,Telefono,Email,IdSalon,MaxPersonas,FechaReserva")] Formulario formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                formulario.FechaCreacion = DateTime.Now;
+                db.Formularios.Add(formulario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", formulario.IdSalon);
+            return View(formulario);
+        }
+
+        // GET: Formularios/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Formulario formulario = db.Formularios.Find(id);
+            if (formulario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", formulario.IdSalon);
+            return View(formulario);
+        }
+
+        // POST: Formularios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Cedula,Nombre,Telefono,Email,IdSalon,MaxPersonas,FechaReserva,FechaCreacion")] Formulario formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(formulario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", formulario.IdSalon);
+            return View(formulario);
+        }
+
+        // GET: Formularios/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Formulario formulario = db.Formularios.Find(id);
+            if (formulario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(formulario);
+        }
+
+        // POST: Formularios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Formulario formulario = db.Formularios.Find(id);
+            db.Formularios.Remove(formulario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Reserva-Salones/Reserva-Salones/Controllers/SalonesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Reserva_Salones;
+
+namespace Reserva_Salones.Controllers
+{
+    public class SalonesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Salones
+        public ActionResult Index()
+        {
+            var salons = db.Salons.Include(s => s.TipoSalon);
+            return View(salons.ToList());
+        }
+
+        // GET: Salones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salon salon = db.Salons.Find(id);
+            if (salon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salon);
+        }
+
+        // GET: Salones/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdTipoSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo");
+            return View();
+        }
+
+        // POST: Salones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Imagen,Nombre,IdTipoSalon,Ubicacion,Disponible")] Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Salons.Add(salon);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdTipoSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", salon.IdTipoSalon);
+            return View(salon);
+        }
+
+        // GET: Salones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salon salon = db.Salons.Find(id);
+            if (salon == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdTipoSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", salon.IdTipoSalon);
+            return View(salon);
+        }
+
+        // POST: Salones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Imagen,Nombre,IdTipoSalon,Ubicacion,Disponible")] Salon salon)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(salon).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdTipoSalon = new SelectList(db.TipoSalons, "Id", "SalonTipo", salon.IdTipoSalon);
+            return View(salon);
+        }
+
+        // GET: Salones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salon salon = db.Salons.Find(id);
+            if (salon == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salon);
+        }
+
+        // POST: Salones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Salon salon = db.Salons.Find(id);
+            db.Salons.Remove(salon);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
