83899f1fd342fc4b390efdd15e17184de13db72a
Tejanita <theexama123@gmail.com>
2022-07-17 11:34:35 -0400

    Agregando formato/ arreglando bugs

--- Reserva-Salones/Reserva-Salones/Model1.Designer.cs
+++ Reserva-Salones/Reserva-Salones/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\carlo\Proyecto-Reserva-Salones\Reserva-Salones\Reserva-Salones\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\LeonelLora\Desktop\Proyectos\Proyecto Final IDS2\Proyecto-Reserva-Salones\Reserva-Salones\Reserva-Salones\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
